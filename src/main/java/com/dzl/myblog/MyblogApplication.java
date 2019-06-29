4507bfec34f9330c42a549887cc762385089e996
Dengzuliang <1243804796@qq.com>
2019-06-29 12:48:50 +0200

    添加ssl证书

--- src/main/java/com/dzl/myblog/MyblogApplication.java
+++ src/main/java/com/dzl/myblog/MyblogApplication.java
@@ -2,16 +2,23 @@ package com.dzl.myblog;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -27,36 +34,51 @@ public class MyblogApplication extends SpringBootServletInitializer {
         return builder.sources(this.getClass());
     }
 
-    /**
-     * http重定向到https
-     * @return
-     */
     @Bean
-    public TomcatServletWebServerFactory servletContainer() {
+
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+
             @Override
+
             protected void postProcessContext(Context context) {
-                SecurityConstraint constraint = new SecurityConstraint();
-                constraint.setUserConstraint("CONFIDENTIAL");
+
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+
                 SecurityCollection collection = new SecurityCollection();
+
                 collection.addPattern("/*");
-                constraint.addCollection(collection);
-                context.addConstraint(constraint);
+
+                securityConstraint.addCollection(collection);
+
+                context.addConstraint(securityConstraint);
+
             }
+
         };
-        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        tomcat.addAdditionalTomcatConnectors(connector());
+
         return tomcat;
+
     }
 
     @Bean
-    public Connector httpConnector() {
+    public Connector connector() {
+
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+
         connector.setScheme("http");
-        //Connector监听的http的端口号
+
         connector.setPort(80);
+
         connector.setSecure(false);
-        //监听到http的端口号后转向到的https的端口号
+
         connector.setRedirectPort(8082);
+
         return connector;
+
     }
 }
